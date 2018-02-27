64830f432f64c6dac6ade7695b45ccf089a3f95a
Andrew Williams-Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2018-02-27 13:52:33 -0700

    Changed findExpr to return a new pointer, rather than a reference

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -49,7 +49,7 @@ Express* SymbolTable::findExpr(std::string key)
 	{
 		try
 		{
-			return &checkTable.top().at(key);
+			return new Express(checkTable.top().at(key).type_ptr, checkTable.top().at(key).raw_val, checkTable.top().at(key).regist);
 		}
 		catch(const std::out_of_range& oor)
 		{
