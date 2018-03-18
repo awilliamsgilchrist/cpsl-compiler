e76a49fdf9117e6cb043bad9b230e052e1d1be7e
Andrew Williams-Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2018-03-17 19:09:04 -0600

    Added condition to track the ghost assignment

--- main.cpp
+++ main.cpp
@@ -232,7 +232,7 @@ void outAssignment(std::string str, Express* expr)
 	
 	Express* oldExpr = symbol_table.findExpr(str);
 	
-	std::cout << oldExpr << std::endl << oldExpr->ref_expr << std::endl;
+	std::cout << oldExpr << std::endl << oldExpr->ref_expr << std::endl << std::cout <<expr;
 	
 	std::string reg = getRegister();
 	if(expr->type_ptr == oldExpr->type_ptr && !expr->regist)
