f8bb6a46a3813307c883f1c51558ba45118b8599
Andrew Williams-Gilchrist <andrew.williams.gilchrist@aggiemail.usu.edu>
2018-03-17 19:30:55 -0600

    Removed redundant cloning from non-pointer symbol_table

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -81,7 +81,7 @@ Express* SymbolTable::findExpr(std::string key)
 	{
 		try
 		{
-			return new Express(checkTable.top().at(key)->type_ptr, checkTable.top().at(key)->raw_val, checkTable.top().at(key)->regist);
+			return checkTable.top().at(key);
 		}
 		catch(const std::out_of_range& oor)
 		{
